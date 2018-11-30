b35639c1f86dd137e57cedd1f3fd3eadef5bb0e7
Nick Bansal <Bansal321@nicks-imac.home>
2018-11-30 11:36:31 +0000

    standard input and standard output fixed

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-const capsule = require('./src/bacteria')
+const { capsule } = require('./src/bacteria')
+const grid = []
 
+process.stdout.write(`\n******************************************\n\n`)
+process.stdout.write('Please enter all your coordinates below, followed by \'end\'\n\n')
+process.stdin.on('data', (data) => {
+    const newData = data.toString().trim()
+    newData.toLowerCase() === 'end' ? process.exit() : grid.push(newData)
+})
+process.on('exit', () => {
+    process.stdout.write(`\n`)
+    const outerArray = []
+    grid.forEach(item => {
+        outerArray.push(item.split(',').map(Number))
+    })
+    capsule(outerArray).forEach(item => {
+        const newString = `${item[0]}, ${item[1]}`
+        process.stdout.write(`${newString}\n`)
+    })
+    process.stdout.write('end\n\n')
+})
