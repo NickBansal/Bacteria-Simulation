0aabee6e2e92e78c115819e044b7a1eab3112f06
Nick Bansal <https://github.com/NickBansal>
2018-12-21 15:57:40 +0000

    Added return new array function

--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,22 @@
 const checker = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]]
 
 const formulateGrid = (grid) => {
-    const newSet = new Set()
+    const removeDuplicateValues = new Set()
     const aliveCells = grid.filter(item => checkAmountOfNeighbours(item, grid) === 2 || checkAmountOfNeighbours(item, grid) === 3)
     const regenCells = regenerateCells(grid)
-
     const nextGenCells = [...aliveCells, ...regenCells]
-
-    nextGenCells.forEach(item => {
-        newSet.add(`${item[0]},${item[1]}`)
+    nextGenCells.forEach(coords => {
+        removeDuplicateValues.add(`${coords[0]},${coords[1]}`)
     })
-    return [...newSet].map(item => item.split(',').map(Number))
-    .filter(item => item[0] >= 0 && item[1] >= 0).sort(sortArrayValues)
+    return returnNewArray([...removeDuplicateValues])
 }
 
 const checkAmountOfNeighbours = (singleCell, remainingCells)  => {
     let neighbours = 0
     checker.forEach(item => {
-        let newArray = []
-        newArray.push(item[0] + singleCell[0], item[1] + singleCell[1])
-        if (remainingCells.some(eachItem => eachItem[0] === newArray[0] && eachItem[1] === newArray[1])) {
+        const xCoord = item[0] + singleCell[0]
+        const yCoord = item[1] + singleCell[1]
+        if (remainingCells.some(eachItem => eachItem[0] === xCoord && eachItem[1] === yCoord)) {
             neighbours++
         }
     })
@@ -31,28 +28,29 @@ const regenerateCells = (grid) => {
     const newSet = new Set()
     grid.forEach(cell => {
         checker.forEach(item => {
-            const newArray = []
-            newArray.push(item[0] + cell[0], item[1] + cell[1])
-            if ((checkAmountOfNeighbours(newArray, grid)) === 3) {
-                result.push(newArray)
+            const xCoord = item[0] + cell[0]
+            const yCoord = item[1] + cell[1]
+            if ((checkAmountOfNeighbours([xCoord, yCoord], grid)) === 3) {
+                result.push([xCoord, yCoord])
             }
         })
     })
     result.forEach(item => {
         newSet.add(`${item[0]},${item[1]}`)
     })
-    return [...newSet].map(item => item.split(',').map(Number))
-    .filter(item => item[0] >= 0 && item[1] >= 0).sort(sortArrayValues)
+    return returnNewArray([...newSet])
 }
 
 const sortArrayValues = (a, b) => a[0] - b[0] !== 0 ? a[0] - b[0] : a[1] - b[1]
 
+const returnNewArray = array => array
+.map(item => item.split(',').map(Number))
+.filter(item => item[0] >= 0 && item[1] >= 0)
+.sort(sortArrayValues)
+
 const printNextGeneration = grid => {
     process.stdout.write(`\n`)
-    const outerArray = []
-    grid.forEach(item => {
-        outerArray.push(item.split(',').map(Number))
-    })
+    const outerArray = grid.map(item => item.split(',').map(Number))
     formulateGrid(outerArray).forEach(item => {
         const newString = `${item[0]}, ${item[1]}`
         process.stdout.write(`${newString}\n`)
